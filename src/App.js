23cd1a2934006d1bb94bb9546d3a85c19b2bf7ab
AhmedAlhassen <makaveli-tag_alsir@yahoo.com>
2023-02-07 17:16:23 +0000

    add user role

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Invoice from "./scenes/neelian/Invoice";
 
 const ROLES = {
   Admin: "admin",
+  User: "user",
 };
 
 function App() {
@@ -28,7 +29,9 @@ function App() {
         <Routes>
           <Route path="invoce" element={<Invoice />} />
           <Route path="/" element={<Layout />}>
-            <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route
+              element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.User]} />}
+            >
               <Route path="/" element={<Dashboard />} />
               <Route path="/" element={<Root />}>
                 <Route path="neelian" element={<Neelian />} />
